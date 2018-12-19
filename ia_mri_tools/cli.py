525bdcc7f4c03e26d3114abf7da4932685b1e2e0
Souheil Inati <souheil.inati@gmail.com>
2018-12-19 13:07:53 -0500

    Add CLI for coil correction

--- ia_mri_tools/cli.py
+++ ia_mri_tools/cli.py
@@ -4,8 +4,7 @@ import click
 import nibabel
 import numpy as np
 from ia_mri_tools.signal_stats import signal_likelihood
-from ia_mri_tools.features import input_normalization, riff
-
+from ia_mri_tools.features import input_normalization, riff, NUM_SCALES, NORMALIZATION_SCALE
 
 def _check_image_compatibility(images):
     im_shape = images[0].shape
@@ -51,125 +50,33 @@ def estimate_signal_mask(input_images, threshold, output):
     click.echo('Wrote signal mask to {}.'.format(output))
 
 
-# @click.command()
-# @click.option('--output', type=click.STRING, default='coil_correction.nii',
-#               help='Output filename for the coil correction.')
-# @click.option('--width', type=click.INT, default=20, help='Smoothing kernel width in pixels.')
-# @click.option('--scale', type=click.FLOAT, default=100.0, help='Scale for the signal value.')
-# @click.argument('input_images', nargs=-1, type=click.STRING)
-# def estimate_coil_correction(input_images, output, scale, width):
-#     """Estimate receive coil intensity correction from one or more images."""
+@click.command()
+@click.option('--output', type=click.STRING, default='out.nii', help='Output filename for the corrected image.')
+@click.option('--scale', type=click.INT, default=NORMALIZATION_SCALE, help='Scale for high pass filtering and smoothing.')
+@click.argument('input_image', type=click.STRING)
+def coil_correction(input_image, scale, output):
+    """Compute and apply receive coil intensity correction."""
 
-#     click.echo('Estimating receive coil intensity correction from {}'.format(input_images))
-#     click.echo('  width: {}, scale: {}'.format(width, scale))
+    # open the images
+    im = nibabel.load(input_image)
 
-#     # open the images
-#     images = [nibabel.load(x) for x in input_images]
+    # load the image data and the coil correction data and apply
+    data = im.get_data().astype('float32')
+    data_corr = input_normalization(data, scale=scale, high_pass_output=False)
 
-#     # all other images must have matching orientation/dimensions/etc.
-#     _check_image_compatibility(images)
+    # write out the result as floating point and preserve the header
+    out_image = type(im)(data_corr, affine=None, header=im.header)
+    out_image.to_filename(output)
 
-#     # sum the input inputs
-#     im_shape = images[0].shape
-#     a = np.zeros(im_shape, dtype=np.float32)
-#     for im in images:
-#         a += im.get_data()
+    click.echo('Wrote coil intensity corrected image to {}.'.format(output))
 
-#     # compute the coil correction and scale it
-#     c = coil_correction(a, width, scale)
-
-#     # write out the result in the same format and preserve the header
-#     out_image = type(images[0])(c, affine=None, header=images[0].header)
-#     out_image.to_filename(output)
-
-#     click.echo('Wrote receive coil intensity correction to {}.'.format(output))
-
-
-# @click.command()
-# @click.option('--correction', type=click.STRING, default='coil_correction.nii',
-#               help='Filename for the coil correction image.')
-# @click.option('--output', type=click.STRING, default='out.nii',
-#               help='Output filename for the corrected image.')
-# @click.argument('input_image', type=click.STRING)
-# def apply_coil_correction(input_image, correction, output):
-#     """Apply receive coil intensity correction."""
-
-#     click.echo('Applying coil intensity correction from {} to {}.'.format(correction, input_image))
-
-#     # open the images
-#     im = nibabel.load(input_image)
-#     corr = nibabel.load(correction)
-
-#     # images must have matching orientation/dimensions/etc.
-#     _check_image_compatibility([im, corr])
-
-#     # load the image data and the coil correction data and apply
-#     out = im.get_data().astype(np.float32) * corr.get_data().astype(np.float32)
-#     print(type(out), out.dtype)
-
-#     # write out the result in the same format and preserve the header
-#     out_image = type(im)(out, affine=None, header=im.header)
-#     out_image.to_filename(output)
-
-#     click.echo('Wrote coil intensity corrected image to {}.'.format(output))
-
-
-# @click.command()
-# @click.option('--output', type=click.STRING, default='out.nii',
-#               help='Output filename for the textures image.')
-# @click.argument('input_image', type=click.STRING)
-# @click.argument('nscales', type=click.INT, default=4)
-# @click.argument('norm_scale', type=click.INT, default=4)
-# def estimate_textures(input_image, nscales, norm_scale, output):
-#     """Estimate 3D multiscale textures."""
-
-#     click.echo('Estimate 3D multiscale textures for {}.'.format(input_image))
-
-#     # open the images
-#     im = nibabel.load(input_image)
-
-#     # compute the textures
-#     t, names = textures(im.get_data(), nscales=nscales, norm_scale=norm_scale)
-#     nfeats = len(t)
-#     out = np.zeros([*t[0].shape, nfeats], t[0].dtype)
-#     for f in range(nfeats):
-#         out[...,f] = t[f]
-
-#     # write out the result in the same format and preserve the header
-#     out_image = type(im)(out, affine=None, header=im.header)
-#     out_image.to_filename(output)
-
-#     click.echo('Wrote 3D multiscale textures to {}.'.format(output))
-
-
-# @click.command()
-# @click.option('--output', type=click.STRING, default='out.nii',
-#               help='Output filename for the normalized image.')
-# @click.argument('input_image', type=click.STRING)
-# @click.option('--width', type=click.INT, default=20, help='Smoothing kernel width in pixels.')
-# def normalize_local(input_image, width, output):
-#     """Local contrast normalization"""
-
-#     click.echo('Computing local contrast normalization for {}.'.format(input_image))
-
-#     # open the images
-#     im = nibabel.load(input_image)
-
-#     # normalize
-#     out = normalize_local_3d(im.get_data(), width)
-
-#     # write out the result and preserve the header
-#     out_image = type(im)(out, affine=None, header=im.header)
-#     out_image.to_filename(output)
-
-#     click.echo('Wrote 3D multiscale textures to {}.'.format(output))
 
 @click.command()
 @click.option('--output', type=click.STRING, default='out.nii',
               help='Output filename for the textures image.')
 @click.argument('input_image', type=click.STRING)
-@click.option('--nscales', type=click.INT, default=4, help='number of spatial scales')
-@click.option('--normalization_scale', type=click.INT, default=None, help='scale for output gain control')
+@click.option('--nscales', type=click.INT, default=NUM_SCALES, help='number of spatial scales')
+@click.option('--normalization_scale', type=click.INT, default=NORMALIZATION_SCALE, help='scale for output gain control')
 def estimate_textures(input_image, nscales, normalization_scale, output):
     """Estimate 3D multiscale textures."""
 
